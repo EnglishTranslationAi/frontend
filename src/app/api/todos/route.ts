9ef4659b22ab065acf994e0708f8e94350818353
Kalinichenko <kalinamono@gmail.com>
2023-11-24 22:15:12 +0300

    delete unused page & routs

--- src/app/api/todos/route.ts
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from "next/server";
-
-const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
-
-export async function GET() {
-  return NextResponse.json({
-      hello:'world'
-  })
-}
