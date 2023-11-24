2bc5f718260ac6cd2959c707624396d16f668d3f
Kalinichenko <kalinamono@gmail.com>
2023-11-24 22:53:18 +0300

    fix building error

--- src/components/ChatLayout/ChatLayout.tsx
+++ src/components/ChatLayout/ChatLayout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {MessagesProvider} from "@/utils/useMessage";
-import MessagesList from "@/components/MessageList/MessageList";
+
 
 const ChatLayout = () => {
     return (
