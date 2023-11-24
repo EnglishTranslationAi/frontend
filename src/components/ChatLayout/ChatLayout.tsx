1f074f280a2fb62b101eaec5ef1b88be391411b4
Kalinichenko <kalinamono@gmail.com>
2023-11-24 22:51:36 +0300

    fix building error

--- src/components/ChatLayout/ChatLayout.tsx
+++ src/components/ChatLayout/ChatLayout.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import {MessagesProvider} from "@/utils/useMessage";
 import MessagesList from "@/components/MessageList/MessageList";
-import MessageForm from "@/components/MessageForm/MessageForm";
 
 const ChatLayout = () => {
     return (
         <MessagesProvider>
-            <MessagesList />
-            <div className="fixed bottom-0 right-0 left-0">
-                <MessageForm />
-            </div>
+            <>sd</>
+
         </MessagesProvider>
     );
 };
